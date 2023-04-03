bf1ec01424f8f478e777867afabde927e9d22533
Chtholly Nota Seniorious <chthollynotaseniorious@ChthollydeMacBook-Air.local>
2023-04-03 12:00:29 +0800

    env_test

---
+++ src/main/java/com/example/tj_music/TjMusicApplication.java
@@ -0,0 +1,13 @@
+package com.example.tj_music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TjMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tj_music/TjMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tj_music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TjMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
