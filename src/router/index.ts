4e52ff2b4c9002ef5ce23158a300d52ebae79ace
jeckes <1368301748@qq.com>
2023-04-14 21:38:59 +0800

    用安民的

---
+++ src/router/index.ts
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  // {
+  //   path: '/Login',
+  //   name: 'Login',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
+  // },
+  // {
+  //   path: '/Register',
+  //   name: 'Register',
+  //   component: () => import('../views/RegisterView.vue')
+  // }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
