cb16ae700746607584667df6634b54badced8722
jeckes <1368301748@qq.com>
2023-04-14 20:51:23 +0800

    初始化前端总分支

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+      <h3> Hello, Corpse Sun!</h3>
+      <img src="../demoImg/profile.jpg" alt="啊哦，马老师照片不见了">
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
