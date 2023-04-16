419123cf41f45df0cb19efb5feea2adc6f0ac1ee
chtholly12 <82920537+horizen-searcher@users.noreply.github.com>
2023-04-16 18:29:15 +0800

    administer_basic

--- src/main/java/com/example/tj_music/controller/administerController.java
+++ src/main/java/com/example/tj_music/controller/administerController.java
@@ -1,5 +1,6 @@
 package com.example.tj_music.controller;
 
+import com.example.tj_music.db.entity.Appeal;
 import com.example.tj_music.db.entity.User;
 import com.example.tj_music.service.administerService;
 import com.example.tj_music.utils.Result;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController // @RestController = @Controller + @ResponseBody (return json)
 public class administerController {
     // please use the logger to print the log
@@ -17,5 +20,44 @@ public class administerController {
     // user service
     @Autowired // auto-inject
     private administerService administerService;
+
+    /**
+     * 获取所有申诉
+     * 用这个API可以获取所有申诉
+     * @param
+     * @return: list of objects: [appealId, ownerStudentNumber,appealContent]
+     */
+    @GetMapping("/allAppeals")
+    public Result allAppeals() {
+        // get all appeals
+        List<List<Object>> appeals = administerService.selectAllAppeals();
+        return Result.success(appeals);
+    }
+
+    /**
+     * 通过申诉
+     * 输入申诉的ID和申诉者的StudentNumber，通过申诉
+     * @param appealId: 需要通过的申诉的ID,之前获取所有申诉的API中可以获取到
+     * @param userStudentNumber: 申诉者的学号，之前获取所有申诉的API中可以获取到
+     * @return Result(success)
+     */
+    @PostMapping("/acceptAppeal")
+    public Result acceptAppeal(@RequestParam("appealId") int appealId, @RequestParam("userStudentNumber") String userStudentNumber) {
+        administerService.acceptAppeal(appealId,userStudentNumber);
+        return Result.success();
+    }
+
+    /**
+     * 拒绝申诉
+     * 输入申诉的ID和拒绝理由，拒绝申诉
+     * @param appealId: 需要拒绝的申诉的ID,之前获取所有申诉的API中可以获取到
+     * @param rejectReason: 拒绝理由
+     * @return Result(success)
+     */
+    @PostMapping("/rejectAppeal")
+    public Result rejectAppeal(@RequestParam("appealId") int appealId, @RequestParam("rejectReason") String rejectReason) {
+        administerService.rejectAppeal(appealId,rejectReason);
+        return Result.success();
+    }
 
 }
--- src/main/java/com/example/tj_music/service/administerService.java
+++ src/main/java/com/example/tj_music/service/administerService.java
@@ -6,6 +6,7 @@ import com.example.tj_music.db.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,5 +16,52 @@ public class administerService {
     @Autowired
     private UserMapper  userMapper;
 
+    // get all the information of appeals
+    public List<List<Object>> selectAllAppeals() {
+        List<List<Object>> appealInfos = new ArrayList<>();
+        // get all appeals and store them in a list
+        List<Appeal> appeals = appealMapper.selectAllAppeal();
+        // for all the appeals in the list, get the information of the user who made the appeal
+        for (Appeal appeal : appeals) {
+            // get the user's student number using the appeal's owner
+            String studentNumber = userMapper.selectUserById(appeal.getAppealOwner()).getUserStudentNumber();
 
+            // get the user's id using the appeal's owner
+            int userId = userMapper.selectUserById(appeal.getAppealOwner()).getUserId();
+            // get appeal's id
+            int appealId = appeal.getAppealId();
+            // get appeal's content
+            String appealContent = appeal.getAppealContent();
+            // store the appeal's id,student number and content in a list,keep the id type as int
+            // and the student number and content type as string
+            // init a empty list
+            List<Object> appealInfo = new ArrayList<>();
+            appealInfo.add(appealId);
+            appealInfo.add(studentNumber);
+            appealInfo.add(userId);
+            appealInfo.add(appealContent);
+            // store the appealInfo list in a new list
+            appealInfos.add(appealInfo);
+        }
+        return appealInfos;
+    }
+
+    // accept the appeal,take appealId and userId as parameters
+    public void acceptAppeal(int appealId, String userStudentNumber) {
+        // get the user's id using the user's student number
+        int userId = userMapper.selectUserByStudentNumber(userStudentNumber).getUserId();
+        // update the user's status to normal
+        userMapper.updateUserStatusById("normal",userId);
+        // delete the appeal
+        appealMapper.deleteAppealById(appealId);
+
+    }
+
+    // reject the appeal,take appealId and reject reason parameter
+    public void rejectAppeal(int appealId, String rejectReason) {
+        // change the appeal's status to rejected
+        appealMapper.updateAppealStatusById("rejected",appealId);
+        // change the appeal's reject reason
+        appealMapper.updateAppealRejectReasonById(rejectReason,appealId);
+    }
 }
