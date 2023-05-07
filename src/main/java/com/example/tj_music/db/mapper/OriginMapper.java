a8a099671db3c1874a8554c756ed0e8baa98eb79
Siri <982017264@qq.com>
2023-05-07 18:21:27 +0800

    add origin

--- src/main/java/com/example/tj_music/db/mapper/OriginMapper.java
+++ src/main/java/com/example/tj_music/db/mapper/OriginMapper.java
@@ -13,10 +13,11 @@ public interface OriginMapper {
     public List<Origin> getAllOrigin();
 
     // Select origin by tag
-    @Select("select * from origin where origin_name like ’%#{originTag}%‘")
-    public Origin selectOriginByTag(String originTag);
+    @Select("select * from origin where origin_name like CONCAT('%', #{originTag, jdbcType=VARCHAR}, '%')")
+    public List<Origin> selectOriginByTag(String originTag);
 
     // Select origin by work id
-    @Select("select * from origin where work_id = #{workId}")
+    @Select("select * from origin inner join work on origin.origin_id = work.work_origin_version" +
+            " where work.work_id = #{workId}")
     public Origin selectOriginByWorkId(Integer workId);
 }
